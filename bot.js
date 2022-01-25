c344677eea4ec2c21f48527bba40302ea922c455
MJMODS <95528082+MJMODS@users.noreply.github.com>
2022-01-25 11:08:35 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,7 +77,7 @@ async function WhiteDevil () {
     conn.version = [3,2147,14];
     const Session = new StringSession();
 
-    conn.browserDescription = ["WhiteDevil-Bot", "Safari", '1.0.0']
+    conn.browserDescription = ["BlackGod-Bot", "Safari", '1.0.0']
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
     var nodb;
